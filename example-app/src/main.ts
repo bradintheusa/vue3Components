7d6913965f9e8f1c3adf0313712b1ae8364a861c
BradInTheUSA <bradintheusa@gmail.com>
2022-04-12 13:35:27 -0400

    first commit

---
+++ example-app/src/main.ts
@@ -0,0 +1,7 @@
+import { createApp } from 'vue'
+import App from './App.vue'
+
+// import plugin from "@anthonygore/vue3-component-library"
+// import plugin from "vue3-component-library"
+
+createApp(App).mount('#app')
---
+++ vue-3-package-skeleton/dist/my-lib.es.js
@@ -0,0 +1,13 @@
+import { defineComponent, h } from "vue";
+const MyComponent = defineComponent({
+  name: "MyComponent",
+  props: {},
+  setup() {
+    return () => h("div", {}, "hello world");
+  }
+});
+const install = (app) => {
+  app.component(MyComponent.name, MyComponent);
+};
+MyComponent.install = install;
+export { MyComponent, MyComponent as default };
